4dba93ebc280c8a8c8eba542f7159c899c8205b6
ankit.kaushik <kaushik89ankit@gmail.com>
2019-06-05 03:07:12 +0530

    Initializing project

---
+++ mobile-app.ws/src/main/java/com/appsdeveloperblog/app/ws/service/Impl/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.appsdeveloperblog.app.ws.service.Impl;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.appsdeveloperblog.app.ws.UserRepository;
+import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
+import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public UserDto createUser(UserDto user) {
+
+		UserEntity userEntity = new UserEntity();
+		BeanUtils.copyProperties(user, userEntity);
+
+		userEntity.setEncryptedPassword("test");
+		userEntity.setUserId("testUserID");
+
+		UserEntity storedUserEntity = userRepository.save(userEntity);
+
+		UserDto returnValue = new UserDto();
+
+		BeanUtils.copyProperties(storedUserEntity, returnValue);
+
+		return returnValue;
+	}
+
+}
---
+++ mobile-app.ws/src/main/java/com/appsdeveloperblog/app/ws/service/UserService.java
@@ -0,0 +1,9 @@
+package com.appsdeveloperblog.app.ws.service;
+
+import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
+
+public interface UserService {
+
+	public UserDto createUser(UserDto user);
+
+}
